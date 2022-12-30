1964cf4392c9a2863ab74d837d604206b0130445
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2022-12-30 19:28:39 +0530

    'save'

--- src/components/Bigslider.jsx
+++ src/components/Bigslider.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
 import bigcss from "../assets/css/components/Bigslider.module.css";
+import {TfiAngleLeft,TfiAngleRight} from 'react-icons/tfi'
 const Bigslider = () => {
   return (
     <Carousel
@@ -12,6 +13,28 @@ const Bigslider = () => {
       centerMode={true}
       centerSlidePercentage={60}
       showStatus={false}
+      renderArrowPrev={(onClickHandler) => {
+        return (
+          <button
+            className={bigcss.prevButton}
+            onClick={onClickHandler}
+            onKeyDown={onClickHandler}
+          >
+            <TfiAngleLeft/>
+          </button>
+        );
+      }}
+      renderArrowNext={(onClickHandler) => {
+        return (
+          <button
+            className={bigcss.nextButton}
+            onClick={onClickHandler}
+            onKeyDown={onClickHandler}
+          >
+            <TfiAngleRight/>
+          </button>
+        );
+      }}
     >
       <div>
         <img
