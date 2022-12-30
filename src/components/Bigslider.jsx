da25bf6981759fbd9ed622bdfb3fa6ed165c7513
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2022-12-30 13:21:51 +0530

    'save'

--- src/components/Bigslider.jsx
+++ src/components/Bigslider.jsx
@@ -4,36 +4,49 @@ import { Carousel } from "react-responsive-carousel";
 import facebook from "../assets/images/facebook.svg";
 import twitter from "../assets/images/twitter.svg";
 import youtube from "../assets/images/youtube.svg";
-import appstore from "../assets/images/appstore.svg"
-import googleplay from "../assets/images/googleplay.svg"
-import firetv from "../assets/images/firetv.png"
-import bigcss from "../assets/css/components/Bigslider.module.css"
+import appstore from "../assets/images/appstore.svg";
+import googleplay from "../assets/images/googleplay.svg";
+import firetv from "../assets/images/firetv.png";
+import bigcss from "../assets/css/components/Bigslider.module.css";
 const Bigslider = () => {
   return (
-      <Carousel className={bigcss.bigSlider}>
-        <div>
-          <img src={facebook} />
-          <p className="legend">Legend 1</p>
-        </div>
-        <div>
-          <img src={twitter}/>
-          <p className="legend">Legend 2</p>
-        </div>
-        <div>
-          <img src={youtube} />
-          <p className="legend">Legend 3</p>
-          <img src={appstore} />
-          <p className="legend">Legend 1</p>
-        </div>
-        <div>
-          <img src={googleplay}/>
-          <p className="legend">Legend 2</p>
-        </div>
-        <div>
-          <img src={firetv} />
-          <p className="legend">Legend 3</p>
-        </div>
-      </Carousel>
+    <Carousel
+      className={bigcss.bigSlider}
+      emulateTouch={true}
+      infiniteLoop={true}
+      showThumbs={false}
+      useKeyboardArrows={true}
+      dynamicHeight={true}
+      centerMode={true}
+      centerSlidePercentage={60}
+      showStatus={false}
+
+    >
+      <div>
+        <img src={"https://v3img.voot.com/resizMedium,w_1280,h_720/v3Storage/assets/16x9_carousel-1672229987397.jpg"} />
+        {/* <p className="legend">Legend 1</p> */}
+      </div>
+      <div>
+        <img src={"https://v3img.voot.com/resizMedium,w_1280,h_720/v3Storage/assets/kyy-16x9-1672120167854.jpg"} />
+        {/* <p className="legend">Legend 2</p> */}
+      </div>
+      <div>
+        <img src={"https://v3img.voot.com/resizMedium,w_1280,h_720/v3Storage/assets/bmtdd-1671514801912.jpg"} />
+        {/* <p className="legend">Legend 3</p> */}
+      </div>
+      <div>
+        <img src={"https://v3img.voot.com/resizMedium,w_1280,h_720/v3Storage/assets/enter_now_-new_bbh_16x9-1671989896924.jpg"} />
+        {/* <p className="legend">Legend 1</p> */}
+      </div>
+      <div>
+        <img src={"https://v3img.voot.com/resizMedium,w_1280,h_720/v3Storage/assets/shark-tank-s2_final_16x9_-1671777101118.jpg"} />
+        {/* <p className="legend">Legend 2</p> */}
+      </div>
+      <div>
+        <img src={"https://v3img.voot.com/resizMedium,w_1280,h_720/v3Storage/assets/durga-aur-charu-16x9-1670301802997.jpg"} />
+        {/* <p className="legend">Legend 3</p> */}
+      </div>
+    </Carousel>
   );
 };
 
