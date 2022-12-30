90f4fbbd064eb129ec97212a14128847124c1385
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2022-12-30 14:23:21 +0530

    'save'

--- src/components/Bigslider.jsx
+++ src/components/Bigslider.jsx
@@ -1,12 +1,6 @@
 import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
-import facebook from "../assets/images/facebook.svg";
-import twitter from "../assets/images/twitter.svg";
-import youtube from "../assets/images/youtube.svg";
-import appstore from "../assets/images/appstore.svg";
-import googleplay from "../assets/images/googleplay.svg";
-import firetv from "../assets/images/firetv.png";
 import bigcss from "../assets/css/components/Bigslider.module.css";
 const Bigslider = () => {
   return (
@@ -21,30 +15,34 @@ const Bigslider = () => {
       centerSlidePercentage={60}
       showStatus={false}
       autoFocus={true}
-
     >
       <div>
         <img src={"https://v3img.voot.com/resizMedium,w_1280,h_720/v3Storage/assets/16x9_carousel-1672229987397.jpg"} />
         {/* <p className="legend">Legend 1</p> */}
       </div>
       <div>
-        <img src={"https://v3img.voot.com/resizMedium,w_1280,h_720/v3Storage/assets/kyy-16x9-1672120167854.jpg"} />
+        <img src={"https://v3img.voot.com/resizMedium,w_1280,h_720/v3Storage/assets/kyy-16x9-1672120167854.jpg"} 
+         onSelect={()=>console.log(2)}/>
         {/* <p className="legend">Legend 2</p> */}
       </div>
       <div>
-        <img src={"https://v3img.voot.com/resizMedium,w_1280,h_720/v3Storage/assets/bmtdd-1671514801912.jpg"} />
+        <img src={"https://v3img.voot.com/resizMedium,w_1280,h_720/v3Storage/assets/bmtdd-1671514801912.jpg"} 
+         onSelect={()=>console.log(3)}/>
         {/* <p className="legend">Legend 3</p> */}
       </div>
       <div>
-        <img src={"https://v3img.voot.com/resizMedium,w_1280,h_720/v3Storage/assets/enter_now_-new_bbh_16x9-1671989896924.jpg"} />
+        <img src={"https://v3img.voot.com/resizMedium,w_1280,h_720/v3Storage/assets/enter_now_-new_bbh_16x9-1671989896924.jpg"} 
+         onSelect={()=>console.log(4)}/>
         {/* <p className="legend">Legend 1</p> */}
       </div>
       <div>
-        <img src={"https://v3img.voot.com/resizMedium,w_1280,h_720/v3Storage/assets/shark-tank-s2_final_16x9_-1671777101118.jpg"} />
+        <img src={"https://v3img.voot.com/resizMedium,w_1280,h_720/v3Storage/assets/shark-tank-s2_final_16x9_-1671777101118.jpg"} 
+         onSelect={()=>console.log(5)}/>
         {/* <p className="legend">Legend 2</p> */}
       </div>
       <div>
-        <img src={"https://v3img.voot.com/resizMedium,w_1280,h_720/v3Storage/assets/durga-aur-charu-16x9-1670301802997.jpg"} />
+        <img src={"https://v3img.voot.com/resizMedium,w_1280,h_720/v3Storage/assets/durga-aur-charu-16x9-1670301802997.jpg"} 
+         onSelect={()=>console.log(6)}/>
         {/* <p className="legend">Legend 3</p> */}
       </div>
     </Carousel>
